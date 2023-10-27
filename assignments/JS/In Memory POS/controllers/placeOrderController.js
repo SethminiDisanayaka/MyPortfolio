277d0427ed6c7a72a4f6ffec4693047fb2a9a3a8
SethminiDissanayake <sethminidissa@gmail.com>
2023-10-27 20:17:41 +0530

    In Memory POS Created

---
+++ assignments/JS/In Memory POS/controllers/placeOrderController.js
@@ -0,0 +1,150 @@
+
+let OrderPreID="O00-00";
+let orderNo=1;
+
+cusIds();
+itemIds();
+setOrderId();
+
+
+function cusIds() {
+    var optionCus = '';
+    for (var i = 0; i < customerDetail.length; i++) {
+        optionCus += '<option value="' + customerDetail[i].customerID + '">' + customerDetail[i].customerID + '</option>';
+    }
+    $('#cmbCusID').append(optionCus);
+}
+
+function itemIds() {
+
+    var optionItem = '';
+    for (var i = 0; i < itemDetails.length; i++) {
+        optionItem += '<option value="' + itemDetails[i].itemID + '">' + itemDetails[i].itemID + '</option>';
+    }
+    $('#cmbItemCode').append(optionItem);
+}
+
+function setOrderId() {
+    $('#InputOID').val(OrderPreID+orderNo);
+    console.log(Number(orderNo));
+}
+$('#cmbCusID').change(function () {
+    for (let i = 0; i < customerDetail.length; i++) {
+        if ($(this).val()==customerDetail[i].customerID){
+            $('#selectedCusName').val(customerDetail[i].customerName);
+            break;
+        }
+    }
+});
+
+$('#cmbItemCode').change(function () {
+    for (let i = 0; i < itemDetails.length; i++) {
+        if ($(this).val()==itemDetails[i].itemID){
+            $('#selectedItemDes').val(itemDetails[i].itemDescription);
+            $('#selectedItemUp').val(itemDetails[i].itemUnitPrice);
+            $('#selectedQty').val(itemDetails[i].itemQty);
+            break;
+        }
+    }
+});
+
+$("#btnAddToCart").click(function () {
+    addToCart();
+});
+
+$('#btnPurchase').click(function () {
+    purchaseOrder();
+});
+function purchaseOrder() {
+    let IdOfOrder = $('#InputOID').val();
+    let dateOfOrder=$('#InputDate').val();
+    let cusIDOfOrder=$('#cmbCusID').val();
+    let cartDetails=orderDetailDb;
+
+    let newOrder=Object.assign({},orderOb);
+    newOrder.id=IdOfOrder;
+    newOrder.date=dateOfOrder;
+    newOrder.customerId=cusIDOfOrder;
+    newOrder.cartDetail=cartDetails;
+
+    orderDb.push(newOrder);
+
+
+
+    orderNo++;
+    setOrderId();
+    console.log(orderNo);
+
+    orderDetailDb=[];
+    alert("order Placed Succesfully !");
+    clearFields();
+}
+
+function addToCart() {
+    let subTotal=0;
+    let oItemID = $("#cmbItemCode").val();
+    let oDesc = $("#selectedCusName").val();
+    let oUnitPrice = $("#selectedItemUp").val();
+    let oQty = $("#Qty").val();
+    let oTotal = oUnitPrice*oQty;
+
+    let newCart = Object.assign({}, cartOb);
+    newCart.IID = oItemID;
+    newCart.IName = oDesc;
+    newCart.IUnitPrice = oUnitPrice;
+    newCart.IQty = oQty;
+    newCart.ITotal = oTotal;
+
+    //add customer record to the customer array
+    orderDetailDb.push(newCart);
+    for (let i = 0; i < itemDetails.length; i++) {
+        if(itemDetails[i].code==oItemID){
+            itemDetails[i].qtyOnHand=itemDetails[i].qtyOnHand-oQty;
+        }
+    }
+
+
+    //create row and add text field values
+    let row=`<tr>
+                    <td>${newCart.IID}</td>
+                    <td>${newCart.IName}</td>
+                    <td>${newCart.IUnitPrice}</td>
+                    <td>${newCart.IQty}</td>
+                    <td>${newCart.ITotal}</td>
+                   </tr>`;
+    //and then append the row to tableBody
+    $("#tBodyPlaceOrder").append(row);
+
+    for (let i = 0; i <= orderDetailDb.length; i++) {
+        subTotal+=orderDetailDb[i].ITotal;
+        $('#inputTotal').val(parseInt(subTotal));
+        console.log(parseInt(subTotal));
+    }
+}
+
+
+$('#inputCash').keydown(function (event){
+
+    if (event.key==='Enter'){
+        let balance = Number($('#inputCash').val())-Number($('#inputTotal').val());
+
+        $('#balance').val(balance);
+        //purchaseOrder();
+    }
+
+});
+function clearFields() {
+    $("#inputCash").val("");
+    $("#InputDate").val("");
+    $("#selectedCusName").val("");
+    $("#inputTotal").val("");
+    $("#selectedItemDes").val("");
+    $("#tBodyPlaceOrder").empty();
+    $("#selectedItemUp").val("");
+    $("#selectedQty").val("");
+    $("#Qty").val("");
+    $("#balance").val("");
+
+}
+
+
---
+++ assignments/JS/In Memory POS/db/db.js
@@ -0,0 +1,15 @@
+let customerDetail=[
+    {customerID:"C00-001",customerName:"Kasun",customerAddress:"Matara",customerContact:"0788249177"},
+    {customerID:"C00-002",customerName:"Saman",customerAddress:"Galle",customerContact:"0767849234"},
+    {customerID:"C00-003",customerName:"Namal",customerAddress:"Jaffna",customerContact:"0745679213"}
+];
+
+let itemDetails= [
+    {itemID:"I00-001",itemDescription:"Sugar",itemUnitPrice:300,itemQty:40},
+    {itemID:"I00-002",itemDescription:"Soap",itemUnitPrice:100,itemQty:200},
+    {itemID:"I00-003",itemDescription:"Biscuit",itemUnitPrice:400,itemQty:100}
+];
+
+let orderDetailDb=[];
+
+let orderDb=[];
